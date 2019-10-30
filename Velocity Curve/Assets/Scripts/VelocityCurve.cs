fcc614bebc1b579a1be909bedf168d04c852e757
JeffreyDufseth <30986885+JeffreyDufseth@users.noreply.github.com>
2019-10-30 13:34:39 -0700

    First pass coding. Still untested.
    Next up is some authoring classes and then some simple testing in the test scene.

---
+++ Velocity Curve/Assets/Scripts/VelocityCurve.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace JeffreyDufseth.VelocityCurve
+{
+    public struct VelocityCurve : IComponentData
+    {
+        public VelocityCurveAxis X;
+        public VelocityCurveAxis Y;
+        public VelocityCurveAxis Z;
+    }
+}
---
+++ Velocity Curve/Assets/Scripts/VelocityCurveAxis.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+
+namespace JeffreyDufseth.VelocityCurve
+{
+    public enum VelocityCurves
+    {
+        Zero,
+        Linear,
+        Quadratic,
+        LinearThenQuadratic
+    }
+
+    public struct VelocityCurveAxis
+    {
+        public VelocityCurves Curve;
+        public float CurrentVelocity;
+
+        public float Acceleration;
+        public float MaximumVelocity;
+        public float MinimumVelocity;
+        public float DelayTimeRemaining;
+    }
+}
