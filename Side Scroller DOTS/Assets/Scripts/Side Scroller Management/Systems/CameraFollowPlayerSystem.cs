eb81ffa6e378b6352df4c03610192faec2cf5c8b
JeffreyDufseth <30986885+JeffreyDufseth@users.noreply.github.com>
2019-11-02 14:42:26 -0700

    Camera Follow
    All basic features are in place. Add some polish.

---
+++ Side Scroller DOTS/Assets/Scripts/Side Scroller Management/Systems/CameraFollowPlayerSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Transforms;
+using JeffreyDufseth.PlayerManagement;
+
+namespace JeffreyDufseth.SideScrollerManagement.Systems
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [UpdateAfter(typeof(TransformSystemGroup))]
+    public class CameraFollowPlayerSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref Player player, ref Translation translation) =>
+            {
+                UnityEngine.Camera.main.transform.position = new UnityEngine.Vector3(translation.Value.x, translation.Value.y, UnityEngine.Camera.main.transform.position.z);
+            });
+        }
+    }
+}
